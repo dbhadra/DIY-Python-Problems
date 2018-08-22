0f2b1731659acc5141128d49d7c9b30896f9fde3
Deepayan Bhadra <dbhadra@umd.edu>
2018-08-21 21:24:27 -0400

    Updated

--- Automating Tasks/Automate.py
+++ Automating Tasks/Automate.py
@@ -6,6 +6,8 @@ Created on Tue Aug 21 11:10:03 2018
 @author: deepayanbhadra
 """
 
+# Pattern Matching
+
 import re
 def password_detect(exp):
     if len(exp) >= 8:        
@@ -18,4 +20,13 @@ def password_detect(exp):
         
     else:
         print("Password length is not strong")
-    
+
+
+def find_websites(exp):
+    webRegex = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)")
+    mo = webRegex.search(exp)
+    if mo != None:
+        print("Website found")
+        mo.group()
+    else:
+        print("No website found")    
